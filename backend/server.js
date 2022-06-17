204ecd953ad35d60fccc837a40f2d73c1d4d0cc0
Jon Flitcroft <jondenise@gmail.com>
2022-06-17 08:51:49 +0100

    Set up routes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+
+// express app
+const app = express();
+const workoutRoutes = require('./routes/workouts');
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// route handler
+app.use('/api/workouts', workoutRoutes);
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT);
+});
